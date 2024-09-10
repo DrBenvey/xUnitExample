15e66a2413db25f81aef2e88372e745b5dbf9d07
Evgenii Goncharov <drbenvey1996@mail.ru>
2024-09-10 19:46:56 +0300

    add logger

--- UnitTests/Tests/RandomHelperTest.cs
+++ UnitTests/Tests/RandomHelperTest.cs
@@ -1,16 +1,24 @@
 ﻿using BusinessLogic.Helpers;
 using System.Text.RegularExpressions;
+using Xunit.Abstractions;
 
 namespace UnitTests.Tests
 {
     public class RandomHelperTest
     {
+        private readonly ITestOutputHelper output;
+
+        public RandomHelperTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
         [Fact]
         public void RandomLatinStringTest()
         {
             var randomString = RandomHelper.RandomLatinString(132);
             bool cond1 = randomString.Length == 132;
             bool cond2 = Regex.IsMatch(randomString, "^[a-zA-Z]*$", RegexOptions.IgnoreCase | RegexOptions.NonBacktracking);
+            output.WriteLine("This is output from {0}", randomString);
             Assert.True(cond1 && cond2);
         }
     }
